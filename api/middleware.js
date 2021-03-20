68833561dc294b266756f6c647a47cc2712290dd
tiencoffee <tiencoffee4@gmail.com>
2021-03-21 05:48:56 +0700

    api/img

--- api/middleware.js
+++ api/middleware.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch')
 
 module.exports = function(req, res, next) {
-	if (req.url.startsWith('/img/')) {
-		url = req.url.replace('/img/', '')
+	if (req.url.startsWith('/api/img/')) {
+		url = req.url.replace('/api/img/', '')
 		url = decodeURIComponent(url)
 		fetch(url)
 			.then(response => response.buffer())
 			.then(buf => {
+				res.setHeader('Access-Control-Allow-Credentials', true)
+				res.setHeader('Access-Control-Allow-Origin', '*')
 				res.setHeader('Content-Type', 'image/svg+xml')
 				res.end(buf)
 			})
